e1602a1e4be1a019b889b53013d0371c4e32ca59
Cristian Pagán Díaz <115019974+cristian-pd@users.noreply.github.com>
2026-05-12 10:38:14 +0300

    Fix

--- src/pages/tcproj.js
+++ src/pages/tcproj.js
@@ -5,7 +5,7 @@ import "../containers/greeting/Greeting.scss"
 import "./proj.scss";
 import "./tcproj.scss";
 import thumbnail from "../assets/new/tcthumbnail.png";
-import videoPath from "../assets/new/tcvide_final.mp4";
+import videoPath from "../assets/new/fixed.mp4";
 
 function TCProj() {
   useLayoutEffect(() => {
