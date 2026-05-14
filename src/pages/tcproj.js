e06d27cde89140ee33db7c96b8e1faf6dbad7973
Cristian Pagán Díaz <115019974+cristian-pd@users.noreply.github.com>
2026-05-14 16:14:11 +0300

    Content update

--- src/pages/tcproj.js
+++ src/pages/tcproj.js
@@ -7,12 +7,12 @@ import "./tcproj.scss";
 import SoftwareSkill from "../components/softwareSkills/SoftwareSkill";
 
 const tcSkills = [
-  { skillName: "gamedev", fontAwesomeClassname: "gameLogo"  },
-  { skillName: "c#",      fontAwesomeClassname: "csLogo"    },
-  { skillName: "unity",   fontAwesomeClassname: "unityLogo" },
-  { skillName: "math",    fontAwesomeClassname: "mathLogo"  },
-  { skillName: "2d-art",  fontAwesomeClassname: "cubeLogo"  },
-  { skillName: "git",     fontAwesomeClassname: "gitLogo"   }
+  { skillName: "game-dev", fontAwesomeClassname: "gameLogo"  },
+  { skillName: "c#",       fontAwesomeClassname: "csLogo"    },
+  { skillName: "unity",    fontAwesomeClassname: "unityLogo" },
+  { skillName: "math",     fontAwesomeClassname: "mathLogo"  },
+  { skillName: "2d-art",   fontAwesomeClassname: "cubeLogo"  },
+  { skillName: "git",      fontAwesomeClassname: "gitLogo"   }
 ];
 
 function TCProj() {
